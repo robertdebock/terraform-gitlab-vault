086f4811e10ad12592e03cf9b7b39f05b9498db9
Robert de Bock <robert@meinit.nl>
2022-11-12 07:36:38 +0100

    Use Zurich and name the resource.

--- files/main.tf
+++ files/main.tf
@@ -1,7 +1,11 @@
 provider "aws" {
-  region = "eu-north-1"
+  region = "eu-central-2"
 }
 
 resource "aws_vpc" "default" {
   cidr_block = "192.168.0.0/16"
+  tags = {
+    Name = "gitlab-vault"
+    owner = "Robert de Bock"
+  }
 }
